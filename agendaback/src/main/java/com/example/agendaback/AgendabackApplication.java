474d95496c08160e1120d0f35015035a71598f77
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-18 11:00:58 +0100

    AGENDA BACK: Project created

---
+++ agendaback/src/main/java/com/example/agendaback/AgendabackApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendabackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendabackApplication.class, args);
+    }
+
+}
---
+++ agendaback/src/test/java/com/example/agendaback/AgendabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendabackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
