edb563277dfec7a44db8f67d73a14c565fe5093e
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-21 12:45:18 +0100

    AGENDA REACT FRONT: Intento de login

--- agendafront/src/components/Header.js
+++ agendafront/src/components/Header.js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from "react-router-dom";
+import {auth} from "../firebase";
 
 const Header = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const signInWithEmailAndPasswordHandler = (event, email, password) => {
+        event.preventDefault();
+        auth.signInWithEmailAndPassword(email, password).catch(error => {
+            setError("ERROR signing in with password and email!");
+            console.error("ERROR signing in with password and email", error);
+        });
+    };
+
+    const onChangeHandler = (event) => {
+        const {name, value} = event.currentTarget;
+
+        if (name === 'userEmail') {
+            setEmail(value);
+        }
+        else if (name === 'userPassword') {
+            setPassword(value);
+        }
+    }
+
     return (
         <div>
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="/contacts" className="navbar-brand">AGENDA</a></div>
+                    <div>
+                        <a href="/contacts" className="navbar-brand">AGENDA</a>
+                    </div>
+                    <div>
+                        <form>
+                            <label htmlFor="userEmail" className="block">EMAIL:</label>
+                            <input
+                                type="email"
+                                className="my-1 p1 w-full"
+                                name="userEmail"
+                                value={email} placeholder="E.g: prueba@gmail.com"
+                                id="userEmail"
+                                onChange={(event) => onChangeHandler(event)}/>
+
+                            <label htmlFor="userPassword" className="block">PASSWORD:</label>
+                            <input
+                                type="password"
+                                className="mt-1 mb-3 p-1 w-full"
+                                name="userPassword"
+                                value={password}
+                                placeholder="your password"
+                                id="userPassword"
+                                onChange={(event) => onChangeHandler(event)}/>
+                            <button
+                                className="bg-green-400 hover:bg-green-500 w-full py-2 text-white"
+                                onClick={(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+                                SIGN IN
+                            </button>
+                        </form>
+                    </div>
                 </nav>
             </header>
         </div>
---
+++ agendafront/src/firebase.js
@@ -0,0 +1,67 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { functions } from "firebase";
+
+// Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZnH4YPRLjKpFF96hliwW8t-xHlPSekC8",
+    authDomain: "prueba-login-isagonzalez.firebaseapp.com",
+    projectId: "prueba-login-isagonzalez",
+    storageBucket: "prueba-login-isagonzalez.appspot.com",
+    messagingSenderId: "426204513378",
+    appId: "1:426204513378:web:14044ee24a96141a37fd3b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => {
+    auth.signInWithPopup(provider);
+};
+
+export const generateUserDocument = async (user, additionalData) => {
+    if (!user) return;
+
+    const userRef = firestore.doc(`users/${user.uid}`);
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+        const { email, displayName, photoURL } = user;
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                photoURL,
+                ...additionalData
+            });
+        } catch (error) {
+            console.error("Error creating user document", error);
+        }
+    }
+    return getUserDocument(user.uid);
+};
+
+const getUserDocument = async uid => {
+    if (!uid) return null;
+    try {
+        const userDocument = await firestore.doc(`users/${uid}`).get();
+
+        return {
+            uid,
+            ...userDocument.data()
+        };
+    } catch (error) {
+        console.error("Error fetching user", error);
+    }
+};
