967d133844d2c76592732bf404c2ef45206c622e
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-24 18:37:07 +0100

    AGENDA REACT FRONT: CSS modify span margin

--- agendafront/src/components/Header.js
+++ agendafront/src/components/Header.js
@@ -8,8 +8,11 @@ const Header = () => {
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div>
-                        <img src="https://cdn-icons-png.flaticon.com/512/6313/6313619.png" alt="AGENDA" />
-                        <a href="/contacts" className="navbar-brand">AGENDA</a>
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/6313/6313619.png" alt="AGENDA"/>
+                        <a href="/contacts" className="navbar-brand" title="Contact list">
+                            AGENDA
+                        </a>
                     </div>
                     <Auth/>
                 </nav>
