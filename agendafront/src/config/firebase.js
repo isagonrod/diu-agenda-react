193652299c19c575160dc934239de2ece26ec5a5
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-25 18:16:33 +0100

    AGENDA REACT BACK: Docker-compose running

--- agendafront/src/config/firebase.js
+++ agendafront/src/config/firebase.js
@@ -1,15 +1,18 @@
 import {firestore, initializeApp} from "firebase/app";
 
-const firebaseApp = initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyDZnH4YPRLjKpFF96hliwW8t-xHlPSekC8",
     authDomain: "prueba-login-isagonzalez.firebaseapp.com",
     projectId: "prueba-login-isagonzalez",
     storageBucket: "prueba-login-isagonzalez.appspot.com",
     messagingSenderId: "426204513378",
     appId: "1:426204513378:web:14044ee24a96141a37fd3b"
-});
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 export default firebaseApp;
+
 export const auth = firebaseApp.auth();
 export const generateUserDocument = async (user, additionalData) => {
     if (!user) return;
--- agendafront/src/login/Auth.js
+++ agendafront/src/login/Auth.js
@@ -2,44 +2,77 @@ import React, {useContext, useState} from "react";
 import "firebase/auth";
 import {auth} from "../config/firebase";
 import {UserContext} from "../providers/UserProvider";
+import {doc, setDoc} from "firebase/firestore";
 
 const Auth = () => {
+
+    const [isRegistry, setIsRegistry] = useState(false);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [role, setRole] = useState('');
     const user = useContext(UserContext);
 
     const signIn = async () => {
         await auth.signInWithEmailAndPassword(email, password);
     }
 
-    const signUp = async () => {
-        await auth.createUserWithEmailAndPassword(email, password);
+    const signUp = async (email, password, role) => {
+        const infoUser = await auth.createUserWithEmailAndPassword(email, password)
+            .then((firebaseUser) => {
+            return firebaseUser;
+        });
+        const docRef = doc(firestore, )
     }
 
     const logout = async () => {
         await auth.signOut();
     }
 
+    function submitHandler(e) {
+        e.preventDefault();
+        const email = e.target.elements.email.value;
+        const password = e.target.elements.password.value;
+        const role = e.target.elements.role.value;
+
+        if (isRegistry) {
+            signIn();
+        } else {
+            signUp(email, password, role)
+                .then();
+        }
+    }
+
     return (
         <div>
-            {
-                user ?
-                    <div>
-                        <p>EMAIL: {email}</p>
-                        <img src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png"/>
-                        <button className="btn btn-primary" onClick={logout}>LOGOUT</button>
-                    </div>
+            <form onSubmit={submitHandler}>
+                <label htmlFor="email">
+                    EMAIL:
+                    <input type="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+                </label>
 
-                    :
-                    <form>
-                        <label htmlFor="email">EMAIL: </label>
-                        <input type="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
-                        <label htmlFor="password">PASSWORD: </label>
-                        <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
-                        <button className="btn btn-secondary" onClick={signUp}>SIGN UP</button>
-                        <button className="btn btn-primary" onClick={signIn}>SIGN IN</button>
-                    </form>
-            }
+                <label htmlFor="password">
+                    PASSWORD:
+                    <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+                </label>
+
+                <label htmlFor="rol">
+                    ROL:
+                    <select id="rol">
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                    </select>
+                </label>
+
+                <input type="submit" value={isRegistry ? "SIGN IN" : "SIGN UP"}/>
+                {/*{*/}
+                {/*    isRegistry ?*/}
+                {/*        <button className="btn btn-primary" onClick={signIn}>SIGN IN</button>*/}
+                {/*        :*/}
+                {/*        <button className="btn btn-secondary" onClick={signUp}>SIGN UP</button>*/}
+                {/*}*/}
+                <button onClick={() => setIsRegistry(isRegistry)}>{isRegistry ? "Log in" : "Register me"}</button>
+            </form>
         </div>
     )
 }
