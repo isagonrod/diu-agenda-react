c976936326ddac6afd0100948a1e71224a271107
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-25 19:08:18 +0100

    AGENDA REACT FRONT: Added conditional in Auth.js

--- agendafront/src/login/Auth.js
+++ agendafront/src/login/Auth.js
@@ -25,29 +25,41 @@ const Auth = () => {
 
     return (
         <div>
-            <form>
-                <label htmlFor="email">EMAIL:</label>
-                <input type="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
 
-                <label htmlFor="password">PASSWORD:</label>
-                <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+            {!user ?
 
-                <label htmlFor="photo">AVATAR:</label>
-                <select id="photo">
-                    <option
-                        value="https://as1.ftcdn.net/v2/jpg/01/21/93/74/1000_F_121937450_E3o8jRG3mKbMaAFprSuNOlyrLraSVVua.jpg">
-                        HER
-                    </option>
-                    <option
-                        value="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg">
-                        HIM
-                    </option>
-                </select>
+                <form>
+                    <label htmlFor="email">EMAIL:</label>
+                    <input type="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
 
-                <input type="submit" className="btn btn-primary" value="SIGN IN" onClick={signIn}/>
-                <input type="submit" className="btn btn-success" value="SIGN UP" onClick={signUp}/>
-                <input type="submit" className="btn btn-danger" value="LOG OUT" onClick={logout}/>
-            </form>
+                    <label htmlFor="password">PASSWORD:</label>
+                    <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
+
+                    <label htmlFor="photo">AVATAR:</label>
+                    <select id="photo">
+                        <option
+                            value="https://as1.ftcdn.net/v2/jpg/01/21/93/74/1000_F_121937450_E3o8jRG3mKbMaAFprSuNOlyrLraSVVua.jpg">
+                            HER
+                        </option>
+                        <option
+                            value="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg">
+                            HIM
+                        </option>
+                    </select>
+
+                    <input type="submit" className="btn btn-primary" value="SIGN IN" onClick={signIn}/>
+                    <input type="submit" className="btn btn-success" value="SIGN UP" onClick={signUp}/>
+                </form>
+
+                    :
+
+                <div>
+                    <p>{email}</p>
+                    <img src={photo} alt={photo.value}/>
+                    <input type="submit" className="btn btn-danger" value="LOG OUT" onClick={logout}/>
+                </div>
+            }
+
         </div>
     )
 }
