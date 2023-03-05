c3d8a03c15f0ed0b74ed416ce1f45750210e04cc
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-03-05 11:42:48 +0100

    AGENDA REACT FRONT: Testing changes in local

--- agendafront/src/http-common.js
+++ agendafront/src/http-common.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
+    //baseURL: "http://agenda.us-east-1.elasticbeanstalk.com/contact",
     baseURL: "http://localhost:8081/contact",
     headers: {
         "Content-type": "application/json"
