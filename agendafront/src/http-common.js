9263bd9360046945252eeaf919a8473eddf29015
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-03-08 13:16:49 +0100

    Entrega final

--- agendafront/src/http-common.js
+++ agendafront/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    //baseURL: "http://agenda.us-east-1.elasticbeanstalk.com/contact",
-    baseURL: "http://localhost:8081/contact",
+    baseURL: "http://agenda.us-east-1.elasticbeanstalk.com/contact",
+    //baseURL: "http://localhost:8081/contact",
     headers: {
         "Content-type": "application/json"
     }
